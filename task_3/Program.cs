305e687e914703252e8bb488d39e0edb487ce6d2
Lyas <Egorlyasotsky@yandex.ru>
2023-04-28 14:14:18 +0400

    task 3 is ready

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -2,3 +2,24 @@
 m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29 */
 
+using System;
+using static System.Console;
+Clear();
+Write("Enter number m: ");
+int n = int.Parse(ReadLine()!);
+Write("Enter number n: ");
+int m = int.Parse(ReadLine()!);
+
+WriteLine($"{Accerman(n, m)}");
+
+
+int Accerman(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return Accerman(n - 1, 1);
+    else
+      return Accerman(n - 1, Accerman(n, m - 1));
+}
