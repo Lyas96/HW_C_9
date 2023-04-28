6f959b239982ff611b01f3f4250363eb63f76278
Lyas <Egorlyasotsky@yandex.ru>
2023-04-28 13:33:27 +0400

    task 1 is ready

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -4,3 +4,21 @@
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1 */
 
+using System;
+using static System.Console;
+Clear();
+Write("Enter number N: ");
+int num = int.Parse(ReadLine()!);
+
+
+Nums1_N(num);
+Write(Nums1_N(num));
+
+string Nums1_N(int num)
+{
+  if (num == 1)
+  {
+    return num.ToString();
+  }
+  return num + ", " + Nums1_N(--num);
+}
